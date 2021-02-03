721a62ae38ebf3e78a49984c1a88d74c5da0807b
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-02-03 00:30:15 -0500

    owner check

--- src/Component/AddNewShop.jsx
+++ src/Component/AddNewShop.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react/';
 import {withRouter} from "react-router-dom";
-import {FormGroup, ArrayInputTags} from "./Widgets.jsx";
+import {FormGroup, ArrayInputTags, NoPermissionPage} from "./Widgets.jsx";
 
 
 
@@ -18,7 +18,8 @@ class AddNewShop extends Component{
                     owner: {classname: "", promptText: "", promptClassname: "", addStatus: false}},
             buttonEnable: {saveButtonEnable: false,
                      categoryAddButtonEnable: false,
-                     ownerAddButtonEnable: false}
+                     ownerAddButtonEnable: false},
+            firstAuthentication: {errorMsg: "", isValid: true},
         }
     }
 
@@ -122,91 +123,6 @@ class AddNewShop extends Component{
 
     }
 
-    // check = (e) => {
-    //
-    //     e.preventDefault();
-    //
-    //     const name = {};
-    //     const category = {};
-    //     const owner = {};
-    //
-    //     let change = {};
-    //
-    //     // check shop name, need to check username.trim() first
-    //     // // when check if username is empty, use trim().
-    //     if(e.target.id === "name") {
-    //         if (this.state.formValue.name.trim() === "") {
-    //             name.classname = "is-invalid"
-    //             name.promptClassname = "invalid-feedback";
-    //             name.promptText = "shop name can't be empty";
-    //         } else if (this.state.formValue.name.length > 15) {
-    //             name.classname = "is-invalid";
-    //             name.promptClassname = "invalid-feedback";
-    //             name.promptText = "should shorter";
-    //         } else {
-    //             name.classname = "is-valid"
-    //             name.promptClassname = "valid-feedback";
-    //             name.promptText = "yes";
-    //         }
-    //         change = name;
-    //     }
-    //
-    //     // check category
-    //     if(e.target.id === "category") {
-    //         if (this.state.formValue.category.trim() === "") {
-    //             category.addStatus = false;
-    //         } else if (this.state.formValue.category.length > 20) {
-    //             category.classname = "is-invalid";
-    //             category.promptClassname = "invalid-feedback";
-    //             category.promptText = "should shorter";
-    //             category.addStatus = false;
-    //         } else if (this.state.formValue.category.indexOf(',') >= 0) {
-    //             category.classname = "is-invalid"
-    //             category.promptClassname = "invalid-feedback";
-    //             category.promptText = "no ',' here";
-    //             category.addStatus = false;
-    //         } else {
-    //             category.classname = "is-valid"
-    //             category.promptClassname = "valid-feedback";
-    //             category.promptText = "it works";
-    //             category.addStatus = true;
-    //         }
-    //         change = category;
-    //     }
-    //
-    //     // check owner
-    //     if (this.state.formValue.owner.trim() === "") {
-    //         owner.addStatus = false;
-    //     } else if(e.target.id === "owner") {
-    //         if (this.state.formValue.owner.length > 30) {
-    //             owner.classname = "is-invalid";
-    //             owner.promptClassname = "invalid-feedback";
-    //             owner.promptText = "should shorter";
-    //             owner.addStatus = false;
-    //         } else if (this.state.formValue.owner.indexOf(',') >= 0) {
-    //             owner.classname = "is-invalid"
-    //             owner.promptClassname = "invalid-feedback";
-    //             owner.promptText = "no ',' here";
-    //             owner.addStatus = false;
-    //         } else {
-    //             owner.classname = "is-valid"
-    //             owner.promptClassname = "valid-feedback";
-    //             owner.promptText = "it works";
-    //             owner.addStatus = true;
-    //         }
-    //         change = owner;
-    //     }
-    //
-    //     // update the state
-    //     this.setState({
-    //         error:{
-    //             ...this.state.error,
-    //             [e.target.id]: change,
-    //         }
-    //     }, () => this.buttonChange())
-    //
-    // }
-
     buttonChange = () => {
         // console.log(this.state.error.category)
         let saveButtonEnable = (this.state.error.name.classname === "is-valid");
@@ -249,94 +165,132 @@ class AddNewShop extends Component{
         }, () => this.check(event))
     }
 
+    submit = (event) => {
+        event.preventDefault();
+        console.log(this.state.formValue)
+    }
+
     render() {
         return(
-            <form className={"d-flex flex-column"}>
-                <h4 className={"d-flex justify-content-center"}
-                    style={{color:"#00635a"}}><strong>add the shop information</strong></h4>
-                <div className={"d-flex justify-content-around"}>
-                    <div>
+                !this.state.firstAuthentication.isValid?
+                    (<NoPermissionPage message={this.state.firstAuthentication.errorMsg}/>):
+                (<form className={"d-flex flex-column"}>
+                    <h4 className={"d-flex justify-content-center"}
+                        style={{color: "#00635a"}}><strong>add the shop information</strong></h4>
+                    <div className={"d-flex justify-content-around"}>
+                        <div>
+                        </div>
+                        <div className={"d-flex flex-column"}>
+                            <FormGroup
+                                id={"name"}
+                                inputValue={this.state.formValue["name"]}
+                                show={"shop name"}
+                                type={"text"}
+                                className={this.state.error.name.classname}
+                                promptClassname={this.state.error.name.promptClassname}
+                                promptText={this.state.error.name.promptText}
+                                change={this.change}/>
+                            <FormGroup
+                                id={"country"}
+                                inputValue={this.state.formValue["country"]}
+                                show={"country"}
+                                type={"text"}
+                                className={"form-control"}
+                                change={this.change}/>
+                            <FormGroup
+                                id={"city"}
+                                inputValue={this.state.formValue["city"]}
+                                show={"city"}
+                                type={"text"}
+                                className={"form-control"}
+                                change={this.change}/>
+                            <FormGroup
+                                id={"street"}
+                                inputValue={this.state.formValue["street"]}
+                                show={"street"}
+                                type={"text"}
+                                className={"form-control"}
+                                change={this.change}/>
+                        </div>
+                        <div className={"d-flex flex-column"}>
+                            <FormGroup
+                                id={"imgUrl"}
+                                // inputValue={this.state.formValue["imgUrl"]}
+                                show={"upload image"}
+                                type={"file"}
+                                className={"form-control"}
+                                change={this.change}/>
+                            <ArrayInputTags show={"categories"}
+                                            id={"category"} // should be the same as the key in the formValue of state
+                                            arrayKey={"categoriesList"}
+                                            arrayValues={this.state.categoriesList}
+                                            inputValue={this.state.formValue["category"]}
+                                            buttonEnable={this.state.buttonEnable.categoryAddButtonEnable}
+                                            promptClassname={this.state.error.category.promptClassname}
+                                            promptText={this.state.error.category.promptText}
+                                            changeFunc={this.change}
+                                            addFunc={this.arrayAdd}
+                                            removeFunc={this.arrayRemove}
+                            />
+                            <ArrayInputTags show={"owners/managers"}
+                                            id={"owner"} // should be the same as the key in the formValue of state
+                                            arrayKey={"ownersList"}
+                                            arrayValues={this.state.ownersList}
+                                            inputValue={this.state.formValue["owner"]}
+                                            buttonEnable={this.state.buttonEnable.ownerAddButtonEnable}
+                                            promptClassname={this.state.error.owner.promptClassname}
+                                            promptText={this.state.error.owner.promptText}
+                                            changeFunc={this.change}
+                                            addFunc={this.arrayAdd}
+                                            removeFunc={this.arrayRemove}
+                            />
+                            {/*<FormGroup id={"owner"} show={"other owners"} type={"text"} className={"form-control"} style={{color:"#00635a"}} change={this.change}/>*/}
+                        </div>
+                        <div></div>
                     </div>
-                    <div className={"d-flex flex-column"}>
-                        <FormGroup
-                            id={"name"}
-                            inputValue={this.state.formValue["name"]}
-                            show={"shop name"}
-                            type={"text"}
-                            className={this.state.error.name.classname}
-                            promptClassname={this.state.error.name.promptClassname}
-                            promptText={this.state.error.name.promptText}
-                            change={this.change}/>
-                        <FormGroup
-                            id={"country"}
-                            inputValue={this.state.formValue["country"]}
-                            show={"country"}
-                            type={"text"}
-                            className={"form-control"}
-                            change={this.change}/>
-                        <FormGroup
-                            id={"city"}
-                            inputValue={this.state.formValue["city"]}
-                            show={"city"}
-                            type={"text"}
-                            className={"form-control"}
-                            change={this.change}/>
-                        <FormGroup
-                            id={"street"}
-                            inputValue={this.state.formValue["street"]}
-                            show={"street"}
-                            type={"text"}
-                            className={"form-control"}
-                            change={this.change}/>
+                    <div className={"d-flex justify-content-center"}>
+                        <button className={"btn btn-primary btn-sm active"}
+                                disabled={!this.state.buttonEnable.saveButtonEnable}
+                                onClick={this.submit}>
+                            save
+                        </button>
                     </div>
-                    <div className={"d-flex flex-column"}>
-                        <FormGroup
-                            id={"imgUrl"}
-                            // inputValue={this.state.formValue["imgUrl"]}
-                            show={"upload image"}
-                            type={"file"}
-                            className={"form-control"}
-                            change={this.change}/>
-                        <ArrayInputTags show={"categories"}
-                                        id={"category"} // should be the same as the key in the formValue of state
-                                        arrayKey={"categoriesList"}
-                                        arrayValues={this.state.categoriesList}
-                                        inputValue={this.state.formValue["category"]}
-                                        buttonEnable={this.state.buttonEnable.categoryAddButtonEnable}
-                                        promptClassname={this.state.error.category.promptClassname}
-                                        promptText={this.state.error.category.promptText}
-                                        changeFunc={this.change}
-                                        addFunc={this.arrayAdd}
-                                        removeFunc={this.arrayRemove}
-                        />
-                        <ArrayInputTags show={"owners/managers"}
-                                        id={"owner"} // should be the same as the key in the formValue of state
-                                        arrayKey={"ownersList"}
-                                        arrayValues={this.state.ownersList}
-                                        inputValue={this.state.formValue["owner"]}
-                                        buttonEnable={this.state.buttonEnable.ownerAddButtonEnable}
-                                        promptClassname={this.state.error.owner.promptClassname}
-                                        promptText={this.state.error.owner.promptText}
-                                        changeFunc={this.change}
-                                        addFunc={this.arrayAdd}
-                                        removeFunc={this.arrayRemove}
-                        />
-                        {/*<FormGroup id={"owner"} show={"other owners"} type={"text"} className={"form-control"} style={{color:"#00635a"}} change={this.change}/>*/}
-                    </div>
-                    <div></div>
-                </div>
-                <div className={"d-flex justify-content-center"}>
-                    <button className={"btn btn-primary btn-sm active"}
-                            disabled={!this.state.buttonEnable.saveButtonEnable}>
-                        save
-                    </button>
-                </div>
-            </form>
+                </form>
+            )
         )
     }
 
     componentDidMount() {
         let token = localStorage.getItem("token");
+        // need to check user's role first
+        fetch("/v1/profile",{
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                "token": token,
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.result === null){
+                    console.log(1);
+                    this.setState({
+                        firstAuthentication: {isValid: false, errorMsg: data.msg}
+                    })
+                }
+                else if(data.result.role !== "owner"){
+                    console.log(2);
+                    this.setState({
+                        firstAuthentication: {isValid: false, errorMsg: "Only owner could access"}
+                    })
+                }
+                else{
+                    console.log(3);
+                    this.setState({
+                        firstAuthentication: {isValid: true, errorMsg: ""}
+                    })
+                }
+            })
     }
 }
 
